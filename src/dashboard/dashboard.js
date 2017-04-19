c60fdbc95318b5888f047f7778b27070193138b1
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-04-18 21:05:06 -0400

    List organizations on dashboard

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -10,18 +10,18 @@
     function DashboardController($http, $scope, AuthService) {
         $scope.logout = AuthService.logout;
 
-        $scope.users = [];
         $scope.organization = {};
+        $scope.organizations = [];
 
         function initialize() {
-            $http.get("/users")
-                .then(function(http) {
-                    $scope.users = http.data;
-                });
-
             $http.get(`/organizations/${sessionStorage.organizationID}`)
                 .then(function(http) {
                     $scope.organization = http.data;
+
+                    $http.get(`/communities/${$scope.organization.communityID}/organizations`)
+                        .then(function(http) {
+                            $scope.organizations = http.data;
+                        });
                 });
 
             $scope.$on("$stateChangeSuccess", function () {
