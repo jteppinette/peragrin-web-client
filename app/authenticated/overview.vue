27be56d36f00c475b42a4c585478e428c3b666bb
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-05-01 22:40:18 -0400

    Add vuetify grid markers instead of custom classes

--- app/authenticated/overview.vue
+++ app/authenticated/overview.vue
@@ -1,28 +1,32 @@
 <template>
-<v-row class="overview">
-  <v-col lg6>
-    <v-card class="elevation-3">
-      <v-card-title class="primary white--text">Organizations</v-card-title>
-      <v-data-table v-model="organizations" :headers="headers" hide-actions>
-        <template slot="items" scope="props">
-          <td class="text-xs-right">{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.address }}</td>
-        </template>
-      </v-data-table>
-    </v-card>
-  </v-col>
-  <v-col lg6 class="map">
-    <v-card v-if="organization" class="elevation-3">
-      <v-card-title class="primary white--text">Map</v-card-title>
-      <v-map :zoom=15 :center="[organization.latitude, organization.longitude]">
-        <v-tilelayer :token="mapboxAPIKey" url="https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={token}"></v-tilelayer>
-        <v-marker :lat-lng="{'lat': organization.latitude, 'lng': organization.longitude}">
-           <v-popup :content="organization.name"></v-popup>
-        </v-marker>
-      </v-map>
-    </v-card>
-  </v-col>
-</v-row>
+<div class="overview">
+
+  <v-row>
+    <v-col sm12 lg6>
+      <v-card class="elevation-3">
+        <v-card-title class="primary white--text">Organizations</v-card-title>
+        <v-data-table v-model="organizations" :headers="headers" hide-actions>
+          <template slot="items" scope="props">
+            <td class="text-xs-right">{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.address }}</td>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-col>
+    <v-col sm12 lg6 class="map">
+      <v-card v-if="organization" class="elevation-3">
+        <v-card-title class="primary white--text">Map</v-card-title>
+        <v-map :zoom=15 :center="[organization.latitude, organization.longitude]">
+          <v-tilelayer :token="mapboxAPIKey" url="https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={token}"></v-tilelayer>
+          <v-marker :lat-lng="{'lat': organization.latitude, 'lng': organization.longitude}">
+             <v-popup :content="organization.name"></v-popup>
+          </v-marker>
+        </v-map>
+      </v-card>
+    </v-col>
+  </v-row>
+
+</div>
 </template>
 
 <script>
@@ -73,14 +77,8 @@ $map-height := 400px;
   display: none;
 }
 
-.overview {
-  .col {
-    padding-bottom: 10px;
-
-    @media screen and (max-width: $grid-breakpoints.lg) {
-      width: 100%;
-    }
-  }
+.overview .col {
+  padding-bottom: 10px;
 }
 
 .map.card {
