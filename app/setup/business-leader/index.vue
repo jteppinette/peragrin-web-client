7cb6691e38e5c1a865cc587047019ea7aba703b8
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-05-15 20:56:33 -0400

    Add business leader join community pstep

--- app/setup/business-leader/index.vue
+++ app/setup/business-leader/index.vue
@@ -6,24 +6,22 @@
     <v-col xs8>
       <v-stepper v-model="step">
         <v-stepper-header>
-          <v-stepper-step step="1" v-bind:complete="step > 1">Setup Business</v-stepper-step>
+          <v-stepper-step step="1" v-bind:complete="step > 1">Business</v-stepper-step>
           <v-divider />
           <v-stepper-step step="2" v-bind:complete="step > 2">Map</v-stepper-step>
           <v-divider />
-          <v-stepper-step step="3" v-bind:complete="step > 3">Connect Community</v-stepper-step>
+          <v-stepper-step step="3" v-bind:complete="step > 3">Communities</v-stepper-step>
           <v-divider />
           <v-stepper-step step="4">Share</v-stepper-step>
         </v-stepper-header>
 
         <v-stepper-content step="1">
-
           <v-subheader><v-icon class="primary--text">info</v-icon> &nbsp; General</v-subheader>
           <v-row row>
             <v-col xs8 offset-xs2>
               <v-text-field v-model="organization.name" type="text" label="Name"></v-text-field>
             </v-col>
           </v-row>
-
           <v-subheader><v-icon class="primary--text">location_on</v-icon> &nbsp; Address</v-subheader>
           <v-row row>
             <v-col xs8 offset-xs2>
@@ -46,7 +44,6 @@
               <v-text-field v-model="organization.country" type="text" label="Country"></v-text-field>
             </v-col>
           </v-row>
-
           <v-btn primary @click.native="setupBusiness">Setup Business</v-btn>
         </v-stepper-content>
 
@@ -57,18 +54,28 @@
                <v-popup :content="organization.name"></v-popup>
             </v-marker>
           </v-map>
-
           <v-btn primary @click.native="updateBusinessLocation">Update Business Location</v-btn>
           <v-btn flat @click.native="step = 3">Continue</v-btn>
         </v-stepper-content>
+
         <v-stepper-content step="3">
-          <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px" />
-          <v-btn primary @click.native="step = 4">Continue</v-btn>
+          <v-container fluid>
+            <v-row>
+              <v-col xs4>
+                <v-subheader>Choose the communities you would like to join</v-subheader>
+              </v-col>
+              <v-col xs8>
+                <v-select label="Communities" :items="communities.available" v-model="communities.selected" auto chips multiple light item-text="name" item-value="name" />
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-btn primary @click.native="joinCommunities">Join Communities</v-btn>
           <v-btn flat @click.native="step = 4">Skip</v-btn>
         </v-stepper-content>
+
         <v-stepper-content step="4">
           <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px" />
-          <v-btn primary router="true" to="/console/overview">Continue to Console</v-btn>
+          <v-btn primary :router="true" to="/console/overview">Continue to Console</v-btn>
         </v-stepper-content>
       </v-stepper>
     </v-col>
@@ -102,16 +109,29 @@ var marker = {
 }
 
 export default {
-  data: () => ({step: 1, organization, marker}),
+  data: () => ({step: 1, organization, marker, communities: {selected: [], available: []}}),
+  mounted: mounted,
   methods: {
     setupBusiness,
     move,
-    updateBusinessLocation
+    updateBusinessLocation,
+    joinCommunities
   },
   components: {
     organizationCard
   }
 };
+
+function mounted() {
+  return this.$http.get('/communities')
+    .then(response => response.json())
+    .then(communities => this.communities.available = communities);
+}
+
+function joinCommunities() {
+  return this.$http.post(`/organizations/${this.organization.id}/communities`, this.communities.selected)
+    .then(() => this.step = 4);
+}
 
 function setupBusiness() {
   return this.$http.post('/organizations', this.organization)
@@ -126,6 +146,8 @@ function move({latlng}) {
 }
 
 function updateBusinessLocation() {
+  if (!this.market.lat || !this.marker.long) return;
+
   this.organization.lat = this.marker.lat;
   this.organization.lon = this.marker.lon;
 
@@ -134,3 +156,12 @@ function updateBusinessLocation() {
     .then(organization => this.organization = organization);
 }
 </script>
+
+<style scoped lang="stylus">
+$map-height := 400px;
+
+.leaflet-container {
+  z-index: 5;
+  height: $map-height - 63 !important;
+}
+</style>
