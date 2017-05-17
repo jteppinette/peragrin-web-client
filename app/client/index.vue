8607037402bce5f69e2b048e2377987a6a06a8fc
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-05-17 15:35:43 -0400

    Update map tile provider; Cleanup client

--- app/client/index.vue
+++ app/client/index.vue
@@ -9,27 +9,27 @@
   <main>
     <v-content>
       <v-progress-linear v-if="loading" :indeterminate="true"></v-progress-linear>
-      <v-map v-if="patron.lat && patron.lon" :zoom="14" :center="[patron.lat, patron.lon]">
-        <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
-        <v-marker v-for="organization in organizations" @l-click="viewOrganization(organization)" :key="organization.id" :lat-lng="{'lat': organization.lat, 'lng': organization.lon}">
+      <v-map v-if="lat && lon" :zoom="14" :center="[lat, lon]">
+        <v-tilelayer url="https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianRlcHBpbmV0dGUtcGVyYWdyaW4iLCJhIjoiY2oxb2phcGY0MDAzajJxcGZvc29wN3ExbyJ9.xtRkiXQAS-P6VOO7B-dEsA"></v-tilelayer>
+        <v-marker v-for="organization in organizations" @l-click="selected = organization" :key="organization.id" :lat-lng="{'lat': organization.lat, 'lng': organization.lon}">
           <v-popup :content="organization.name"></v-popup>
         </v-marker>
       </v-map>
     </v-content>
-    <v-sidebar v-if="organization.id" right class="scroll-y" scroll-y>
+    <v-sidebar v-if="selected.id" right class="scroll-y" scroll-y>
 
       <v-subheader>General</v-subheader>
       <v-container>
-        <p><strong class="white--text">{{ organization.name }}</strong></p>
+        <p><strong class="white--text">{{ selected.name }}</strong></p>
       </v-container>
 
       <v-divider></v-divider>
 
       <v-subheader>Address</v-subheader>
       <blockquote style="color: white">
-        {{ organization.street }} <br/>
-        {{ organization.city }} {{ organization.state }} {{ organization.zip }} <br/>
-        {{ organization.country }}
+        {{ selected.street }} <br/>
+        {{ selected.city }} {{ selected.state }} {{ selected.zip }} <br/>
+        {{ selected.country }}
       </blockquote>
 
       <v-list subheader>
@@ -46,59 +46,43 @@
 </template>
 
 <script>
-import organizationCard from 'common/organization/card';
-
-const coords = {
-  lon: -84.30444, lat: 33.79023
-};
-
-var patron = {
-  lon: 0, lat: 0
-};
-
 const promotions = [
   {name: '10% Off Discount'}, {name: 'BOGO Free'}
 ];
 
 export default {
-  data: () => ({patron, organizations: [], loading: true, organization: {}, promotions}),
-  mounted: function() {
-    coordinates.call(this)
-    this.$http.get('/organizations')
-      .then(response => response.json())
-      .then(organizations => this.organizations = organizations);
-  },
-  methods: {
-    viewOrganization: function(organization) {
-      this.organization = organization;
-    }
-  },
-  components: {
-    organizationCard
-  }
+  data: () => ({organizations: [], loading: true, selected: {}, promotions, lon: 0, lat: 0}),
+  mounted
 };
 
-function coordinates() {
-  if (!navigator.geolocation) {
-    this.patron.lon = coords.lon;
-    this.patron.lat = coords.lat;
-    this.loading = false;
-  }
-  navigator.geolocation.getCurrentPosition(function({coords}) {
-    this.patron.lon = coords.longitude;
-    this.patron.lat = coords.latitude;
-    this.loading = false;
-  }.bind(this));
+function mounted() {
+  geo()
+    .then(({lon, lat}) => {
+      this.lon = lon;
+      this.lat = lat;
+      this.loading = false;
+    });
+  this.$http.get('/organizations')
+    .then(response => response.json())
+    .then(organizations => this.organizations = organizations);
+}
+
+function geo() {
+  const fb = {lon: -84.30444, lat: 33.79023};
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) return resolve({lon: fb.lon, lat: fb.lat});
+    navigator.geolocation.getCurrentPosition(({coords: {longitude, latitude}}) => {
+      return resolve({lon: longitude, lat: latitude});
+    }, () => {
+      return resolve({lon: fb.lon, lat: fb.lat});
+    });
+  });
 }
 </script>
 
 <style scoped lang="stylus">
 .toolbar__title {
   font-family: 'Fredoka One', Roboto, sans-serif;
-}
-
-body {
-  height: 100vh;
 }
 
 .leaflet-container {
