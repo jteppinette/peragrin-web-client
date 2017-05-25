4c9fcb524532f73ff2f46c7cda6a98d7e5fcb7d4
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-05-25 17:59:57 -0400

    Fix contextual geo lookup on mobile

--- app/common/organization/card.vue
+++ app/common/organization/card.vue
@@ -56,7 +56,7 @@
                 <v-icon class="primary--text">location_on</v-icon>
               </v-list-tile-action>
               <v-list-tile-content>
-                <v-list-tile-title><a :href="'geopoint:'+organization.lat+','+organization.lon">{{ organization.street }}</a></v-list-tile-title>
+                <v-list-tile-title><a :href="'geo:'+organization.lat+','+organization.lon">{{ organization.street }}</a></v-list-tile-title>
                 <v-list-tile-sub-title>{{ organization.city}} , {{ organization.state }} {{ organization.zip }}</v-list-tile-sub-title>
               </v-list-tile-content>
             </v-list-tile>
