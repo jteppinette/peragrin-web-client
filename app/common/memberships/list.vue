a79ee9ef122ab04b78128cc02893bd589fe193b2
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-06-08 15:50:58 -0400

    Add create membership error handling

--- app/common/memberships/list.vue
+++ app/common/memberships/list.vue
@@ -4,18 +4,19 @@
   <v-dialog v-model="createMembershipDialog" width="400px">
     <v-btn floating slot="activator" class="white"><v-icon dark>add</v-icon></v-btn>
     <v-card>
-      <v-card-row><v-card-title class="primary">Add Membership</v-card-title></v-card-row>
+      <v-card-row><v-card-title class="primary">Create Membership</v-card-title></v-card-row>
+      <v-alert error dismissible v-model="createMembershipError">{{ createMembershipMsg }}</v-alert>
       <v-card-row>
         <v-card-text>
-          <form class="pt-3" @keyup.enter="createMembership">
-            <v-text-field v-model="membership.name" label="Name"></v-text-field>
-            <v-text-field v-model="membership.description" label="Description" rows="1" multi-line></v-text-field>
+          <form @submit.prevent="createMembership">
+            <v-text-field v-model="membership.name" :error="createMembershipError" label="Name"></v-text-field>
+            <v-text-field v-model="membership.description" :error="createMembershipError" label="Description" rows="1" multi-line></v-text-field>
+            <div class="right">
+              <v-btn flat @click.native="createMembershipDialog = false">Close</v-btn>
+              <v-btn primary type="submit" class="white--text">Create Membership</v-btn>
+            </div>
           </form>
         </v-card-text>
-      </v-card-row>
-      <v-card-row actions>
-        <v-btn flat @click.native="createMembershipDialog = false">Close</v-btn>
-        <v-btn primary class="white--text" @click.native="createMembership">Save</v-btn>
       </v-card-row>
     </v-card>
   </v-dialog>
@@ -23,7 +24,6 @@
   <v-expansion-panel class="elevation-0">
     <v-expansion-panel-content v-for="membership in memberships" :key="membership.id">
       <div slot="header"><strong>{{ membership.name }}</strong><br/><small>{{ membership.description }}</small></div>
-
       <div class="pa-2">
         <v-dialog v-model="addAccountDialog[membership.id]" width="400px">
           <v-btn block primary light slot="activator" class="ma-0">Add Account</v-btn>
@@ -78,6 +78,8 @@ export default {
     account,
     membership,
     createMembershipDialog: false,
+    createMembershipError: false,
+    createMembershipMsg: '',
     addAccountDialog: {},
     addAccountError: false,
     addAccountMsg: '',
@@ -110,7 +112,8 @@ function initializeMembershipAccounts(membership) {
 function createMembership() {
   return this.$http.post(`/communities/${this.communityID}/memberships`, this.membership)
     .then(() => this.createMembershipDialog = false)
-    .then(initialize);
+    .then(initialize)
+    .catch(({data}) => this.createMembershipError = !!(this.createMembershipMsg = data && data.msg ? data.msg : 'unknown error'));
 }
 
 function addAccount(membership) {
