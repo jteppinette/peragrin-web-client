4757b59245cff3a640c43a63d5e306c6afcdf279
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-06-08 22:30:52 -0400

    Add form error handling to account update

--- app/profile/index.vue
+++ app/profile/index.vue
@@ -2,14 +2,17 @@
 <v-container fluid class="pa-3">
   <v-layout row wrap>
 
-    <v-flex xs12>
+    <v-flex xs12 sm8 md6 lg4>
       <v-card>
         <v-card-title class="primary">Account</v-card-title>
-        <form @keyup.enter="login" class="pa-3">
-          <v-text-field v-model="email" prepend-icon="mail" type="email" label="Email"></v-text-field>
-          <v-text-field v-model="password" prepend-icon="lock" type="password" label="Password"></v-text-field>
-          <v-btn @click.native="update" class="white--text" block primary>{{ success ? 'Success - Account Updated' : 'Update Account' }}</v-btn>
-        </form>
+        <v-alert error dismissible v-model="error">{{ msg }}</v-alert>
+        <v-container fluid>
+          <form @submit.prevent="update" novalidate>
+            <v-text-field v-model="email" :error="error" prepend-icon="mail" type="email" label="Email"></v-text-field>
+            <v-text-field v-model="password" :error="error" prepend-icon="lock" type="password" label="Password"></v-text-field>
+            <v-btn type="submit" class="white--text" block primary>Update Account</v-btn>
+          </form>
+        </v-container>
       </v-card>
     </v-flex>
 
@@ -19,7 +22,7 @@
 
 <script>
 export default {
-  data: () => ({email: '', password: '', success: false}),
+  data: () => ({email: '', password: '', success: false, error: false, msg: ''}),
   methods: {update},
   beforeRouteEnter (to, from, next) {
     next(sessionStorage.userID ? undefined : {path: '/auth/login', query: {redirect: to.fullPath}});
@@ -28,12 +31,7 @@ export default {
 
 function update() {
   return this.$store.dispatch('update', {email: this.email, password: this.password})
-    .then(() => this.success = true);
+    .then(() => this.success = true)
+    .catch(({data}) => this.error = !!(this.msg = data && data.msg ? data.msg : 'unknown error'));
 }
 </script>
-
-<style scoped lang="stylus">
-.card {
-  max-width: 400px;
-}
-</style>
